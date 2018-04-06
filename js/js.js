87df4947dce92740192b95e745abe24f3b03be7f
BlueberryandMuffin <37666009+BlueberryandMuffin@users.noreply.github.com>
2018-04-06 15:12:21 -0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,4 +1,10 @@
 
+function mytime() {
+	var a = new Date();
+	document.getElementById('showTime').innerHTML = a.getFullYear()+'年'+(a.getMonth()+1) +'月'+ a.getDate()+'日' + '星期' + '日一二三四五六'.charAt(new Date().getDay())+ a.getHours()+':'+a.getMinutes()+':'+a.getSeconds();
+}
+setInterval(mytime, 1000);
+
 function ut2(){
 	document.getElementById('ut').style.display = 'none';
 }
