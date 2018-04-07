be93fa67bd6538121f6b9d44fc8873a6c3655fd7
BlueberryandMuffin <37666009+BlueberryandMuffin@users.noreply.github.com>
2018-04-07 07:34:51 -0400

    Update js.js

--- z1/js/js.js
+++ z1/js/js.js
@@ -1,4 +1,27 @@
+window.onload=function(){
+	function mytime() {
+		var a = new Date();
+		if (a.getHours() < 10) {
+			Hours = "0" + a.getHours()
+		}else{
+			Hours=a.getHours()
+		}
+		if (a.getMinutes() < 10) {
+			Minutes = "0" + a.getMinutes()
+		}else{
+			Minutes=a.getMinutes()
+		}
+		if (a.getSeconds() < 10) {
+			Seconds = "0" + a.getSeconds()
+		}else{
+			Seconds=a.getSeconds()
+		}
+		document.getElementById('showTime').innerHTML = a.getFullYear() + '年' + (a.getMonth() + 1) + '月' + a.getDate() + '日' + '星期' + '日一二三四五六'.charAt(new Date().getDay()) + Hours + ':' + Minutes + ':' + Seconds;
+	}
+	setInterval(mytime, 1000);
 
-function ut2(){
-	document.getElementById('ut').style.display = 'none';
+
+	function ut2() {
+		document.getElementById('ut').style.display = 'none';
+	}	
 }
